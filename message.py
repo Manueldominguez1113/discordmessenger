c65c13ff784d661d341264940e4a27bb5e156045
Manuel "DZ" Dominguez <94648260+Manueldominguez1113@users.noreply.github.com>
2024-12-30 11:00:16 -0500

    Add files via upload

--- message.py
+++ message.py
@@ -1,18 +1,3 @@
-# import sys
-# import http.client
-#
-# def send(message):
-#     webhookurl = "https://discord.com/api/webhooks/1323292248628265042/ii0-TUjd8o2-IwQBLdDffHuLi19oYU6ho1_VOuCFVkin0cTHKFaYbSWeDpfFVEHhec36"
-#     formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n" + message + "\r\n------:::BOUNDARY:::--"
-#
-#     connection = http.client.HTTPSConnection("discordapp.com")
-#     connection.request("POST", webhookurl, formdata, {"Content-Type": "multipart/form-data; boundary=----:::BOUNDARY:::", "cache-control": "no-cache"})
-#     response = connection.getresponse()
-#     result = response.read()
-#
-#     return result.decode("utf-8")
-# print(send(sys.argv[0]))
-#
 import sys
 import requests
 import keys
